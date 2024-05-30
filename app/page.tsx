1a299b20bcf88864379ab16cf169cdb398f8ae75
pausm99 <pausm99@gmail.com>
2024-05-30 19:07:28 +0200

    added form

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return(
-    <main>
+    <main className="p-8">
       <h1>Todo list</h1>
+      <form>
+        <input type="text" name="what" className="border border-white p-1 mr-2 rounded"/>
+        <button>Add todo</button>
+      </form>
     </main>
   )
 }
